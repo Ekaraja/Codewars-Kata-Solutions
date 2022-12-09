72d57c6a0d602efaa97c6733d4add24f50c230ec
Ekaraja <75461147+Ekaraja@users.noreply.github.com>
2022-12-09 13:39:02 +0100

    Removes duplicates from an array of numbers

--- 7kyu - Remove duplicates from list.js
+++ 7kyu - Remove duplicates from list.js
@@ -1,7 +1,8 @@
 //Define a function that removes duplicates from an array of numbers and returns it as a result.
-
 //The order of the sequence has to stay the same.
 
 function distinct(a) {
     return Array.from(new Set(a));
 }
+
+// My solution relies on the Set object which only allows unique values, this automatically removes any duplicate numbers
